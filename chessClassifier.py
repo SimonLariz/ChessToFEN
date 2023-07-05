28493362a72054cb582d25f37b667dcdeb3fe096
simonlariz <simonlariz@gmail.com>
2023-07-05 15:31:00 -0700

    adding files

---
+++ chessClassifier.py
@@ -0,0 +1,248 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+from torch.utils.data import Dataset
+from torchvision.io import read_image, ImageReadMode
+from torchvision.models import resnet50, ResNet50_Weights
+import matplotlib.pyplot as plt
+import sklearn.metrics
+from PIL import Image
+
+import glob
+import os
+
+transform = transforms.Compose(
+    [transforms.Resize((128, 128)), transforms.RandomHorizontalFlip(p=0.5)]
+)
+
+train_dataset = torchvision.datasets.ImageFolder(root="Images", transform=transform)
+
+
+class CustomDataset(Dataset):
+    def __init__(self, root_dir, transform):
+        self.transform = transform
+        self.image_paths = []
+        for ext in ["png"]:
+            self.image_paths += glob.glob(os.path.join(root_dir, "*", f"*.{ext}"))
+        self.image_paths.sort()
+        class_set = set()
+        for path in self.image_paths:
+            class_set.add(os.path.dirname(path))
+        self.class_lbl = {cls: i for i, cls in enumerate(sorted(list(class_set)))}
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        img = read_image(self.image_paths[idx], ImageReadMode.RGB).float()
+        cls = os.path.dirname(self.image_paths[idx])
+        label = self.class_lbl[cls]
+
+        return self.transform(img), torch.tensor(label)
+
+
+def create_model():
+    """Creates a model with a pretrained ResNet50 backbone and a custom head"""
+    # Instantiate the dataset
+    dataset = CustomDataset(root_dir="Images/", transform=transform)
+    print("Dataset instantiated")
+
+    # Create the splits
+    splits = [0.8, 0.1, 0.1]
+    split_sizes = []
+    for split in splits[:-1]:
+        split_sizes.append(int(split * len(dataset)))
+    split_sizes.append(len(dataset) - sum(split_sizes))
+    print("Splits created")
+
+    # Split the dataset using torch.utils.data.random_split
+    train_dataset, val_dataset, test_dataset = torch.utils.data.random_split(
+        dataset, split_sizes
+    )
+    print(test_dataset)
+    print("Dataset split")
+
+    # Define the data loaders
+    dataloaders = {
+        "train": torch.utils.data.DataLoader(train_dataset, batch_size=4, shuffle=True),
+        "val": torch.utils.data.DataLoader(val_dataset, batch_size=4, shuffle=False),
+        "test": torch.utils.data.DataLoader(test_dataset, batch_size=4, shuffle=False),
+    }
+    print("Data loaders defined")
+
+    # Initalize the model
+    model = resnet50(weights=ResNet50_Weights.IMAGENET1K_V1)
+    print("Model initialized")
+
+    model.fc = torch.nn.Sequential(
+        torch.nn.Linear(2048, 256),
+        torch.nn.ReLU(),
+        torch.nn.Linear(256, 13),
+    )
+
+    # Move the model to the GPU if available
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model.to(device)
+    print("Model moved to", device)
+
+    # Freeze parameters of pretrained model
+    for param in model.parameters():
+        param.requires_grad = False
+    for param in model.fc.parameters():
+        param.requires_grad = True
+
+    # Define the loss function
+    criterion = torch.nn.CrossEntropyLoss()
+    print("Loss function defined")
+
+    # Define the optimizer
+    optimizer = torch.optim.Adam(model.fc.parameters(), lr=0.0001)
+
+    # Define metrics
+    metrics = {
+        "train": {"loss": [], "acc": []},
+        "val": {"loss": [], "acc": []},
+    }
+
+    # Train the model
+    for epoch in range(300):
+        epoch_metrics = {
+            "train": {"loss": 0, "accuracy": 0, "count": 0},
+            "val": {"loss": 0, "accuracy": 0, "count": 0},
+        }
+        print(f"Epoch {epoch + 1}")
+
+        for phase in ["train", "val"]:
+            print(f"Phase: {phase}")
+            for images, labels in dataloaders[phase]:
+                optimizer.zero_grad()
+
+                with torch.set_grad_enabled(phase == "train"):
+                    output = model(images.to(device))
+                    # One-hot encode the label
+                    ohe_label = torch.nn.functional.one_hot(labels, num_classes=13)
+
+                    loss = criterion(output, ohe_label.float().to(device))
+
+                    correct_preds = labels.to(device) == torch.argmax(output, dim=1)
+                    accuracy = (correct_preds).sum() / len(labels)
+
+                if phase == "train":
+                    loss.backward()
+                    optimizer.step()
+
+                epoch_metrics[phase]["loss"] += loss.item()
+                epoch_metrics[phase]["accuracy"] += accuracy.item()
+                epoch_metrics[phase]["count"] += 1
+
+            ep_loss = epoch_metrics[phase]["loss"] / epoch_metrics[phase]["count"]
+            ep_acc = epoch_metrics[phase]["accuracy"] / epoch_metrics[phase]["count"]
+
+            print(f"Loss: {ep_loss}, Accuracy: {ep_acc}")
+
+            metrics[phase]["loss"].append(ep_loss)
+            metrics[phase]["acc"].append(ep_acc)
+
+    """# Visualize the metrics
+    for phase in metrics:
+        for metric in metrics[phase]:
+            metric_data = metrics[phase][metric]
+            plt.plot(range(len(metric_data)), metric_data)
+            plt.xlabel("Epoch")
+            plt.ylabel(f"{phase} {metric}")
+            plt.show()"""
+
+    # Test the model
+    preds = []
+    actual = []
+
+    total_loss = total_acc = count = 0
+
+    for images, labels in dataloaders["test"]:
+        with torch.set_grad_enabled(False):
+            output = model(images.to(device))
+            # One-hot encode the label
+            ohe_label = torch.nn.functional.one_hot(labels, num_classes=13)
+            out_labels = torch.argmax(output, dim=1)
+
+            total_loss += criterion(output, ohe_label.float().to(device))
+            total_acc += (labels.to(device) == out_labels).sum() / len(labels)
+            count += 1
+
+        preds += out_labels.tolist()
+        actual += labels.tolist()
+
+    print(f"Test loss: {total_loss/count}, Test accuracy: {total_acc/count}")
+
+    """# Confusion matrix
+    class_labels = ["bishop", "king", "knight", "pawn", "queen", "rook"]
+    cm = sklearn.metrics.confusion_matrix(actual, preds)
+    disp = sklearn.metrics.ConfusionMatrixDisplay(
+        confusion_matrix=cm, display_labels=class_labels
+    )
+
+    disp.plot()
+    plt.show()"""
+
+    # Save the model
+    torch.save(model, "chess_piece_classifier_refined.pt")
+    print("Model saved")
+
+
+def load_model():
+    """Loads the model"""
+    model = torch.load("chess_piece_classifier_refined.pt")
+    model.eval()
+    return model
+
+
+def main():
+    # create_model()
+    # create_model()
+
+    chess_board = []
+    # Load the model
+    model = load_model()
+    print("Model loaded")
+
+    # Create dataset
+    my_dataset = CustomDataset(root_dir="data/", transform=transform)
+    print("Dataset instantiated")
+
+    # Create dataloader
+    my_dataloader = torch.utils.data.DataLoader(my_dataset, batch_size=1, shuffle=False)
+    # Print labels
+    my_dataloader.dataset
+    print("Dataloader instantiated")
+
+    # Move the model to the GPU if available
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    class_labels = [
+        "bishop_dark",
+        "bishop_light",
+        "empty",
+        "king_dark",
+        "king_light",
+        "knight_dark",
+        "knight_light",
+        "pawn_dark",
+        "pawn_light",
+        "queen_dark",
+        "queen_light",
+        "rook_dark",
+        "rook_light",
+    ]
+    # Get batch
+    for images, labels in my_dataloader:
+        # Get predictions
+        with torch.no_grad():
+            output = model(images.to(device))
+            out_labels = torch.argmax(output, dim=1)
+            print(class_labels[out_labels])
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,74 @@
+import pyautogui
+from PIL import Image
+import string
+import cv2
+
+
+def capture_screen():
+    """Captures the screen and saves it as a png file"""
+    screen = pyautogui.screenshot()
+    screen.save("screenshot.png")
+
+
+def find_board(image):
+    """Attempts to find a chess board in the image"""
+    # Resize the image to 50% of its original size
+    image = cv2.resize(image, (0, 0), fx=0.5, fy=0.5)
+    # Grayscale the image using OpenCV
+    grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Apply erosion to the image
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    eroded_image = cv2.erode(grayscale_image, kernel)
+    # Apply dilation to the image
+    dilated_image = cv2.dilate(eroded_image, kernel)
+    # Canny edge detection
+    canny_image = cv2.Canny(dilated_image, 100, 200)
+    # Find contours
+    contours, hierarchy = cv2.findContours(
+        canny_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE
+    )
+    # Crop image around the largest contour
+    largest_contour = max(contours, key=cv2.contourArea)
+    x, y, w, h = cv2.boundingRect(largest_contour)
+    cropped_image = image[y : y + h, x : x + w]
+    # If cropped image is too small, no board was found
+    if cropped_image.shape[0] < 100 or cropped_image.shape[1] < 100:
+        print("No board found")
+        return
+    return cropped_image
+
+
+def crop_out_pieces(image):
+    """Given chessboard image, crops out pieces and returns a list of images"""
+    # Find length of each square
+    square_length = image.shape[0] / 8
+    # Find width of each square
+    square_width = image.shape[1] / 8
+
+    # Define mapping from index to square
+    file_names = string.ascii_lowercase[:8]
+    rank_names = string.digits[8:0:-1]
+    for i in range(8):
+        for j in range(8):
+            square = image[
+                int(i * square_length) : int((i + 1) * square_length),
+                int(j * square_width) : int((j + 1) * square_width),
+            ]
+            file_name = file_names[j] + rank_names[i]
+            cv2.imwrite("data/" + file_name + ".png", square)
+
+    print("Done")
+
+
+def main():
+    capture_screen()
+    current_image = cv2.imread("screenshot.png")
+    board_img = find_board(current_image)
+    if board_img is not None:
+        crop_out_pieces(board_img)
+    print("Done")
+    return 0
+
+
+if __name__ == "__main__":
+    main()
